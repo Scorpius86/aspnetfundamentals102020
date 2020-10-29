278d5af727f4b2d570cada22beab6a399a9cf525
Erick Arostegui Cunza <scorpius86@hotmail.com>
2020-10-28 21:11:44 -0500

    Session 02

---
+++ 02 - Net Core Fundamentals/AspNet.Fundamentals/AspNet.Fundamentals.Web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNet.Fundamentals.Web.Models;
+
+namespace AspNet.Fundamentals.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            PersonViewModel fabiana = new PersonViewModel("Fabiana", 12);
+            List<AddressViewModel> adresses = new List<AddressViewModel>();
+            adresses.Add(new AddressViewModel( 1, "San Isidro"));
+            adresses.Add(new AddressViewModel(1, "Miraflores"));
+            fabiana.Addresses = adresses;
+
+            return View(fabiana);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 02 - Net Core Fundamentals/AspNet.Fundamentals/AspNet.Fundamentals.Web/Models/PersonViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNet.Fundamentals.Web.Models
+{
+    public class PersonViewModel
+    {
+        public PersonViewModel()
+        {
+
+        }
+        public PersonViewModel(string firstName, int age)
+        {
+            FirstName = firstName;
+            Age = age;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<AddressViewModel> Addresses { get; set; }
+    }
+}
