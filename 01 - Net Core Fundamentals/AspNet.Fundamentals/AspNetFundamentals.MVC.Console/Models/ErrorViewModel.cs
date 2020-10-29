5086f7dcc0c583184dbd12bbfaf8c99f94434ce3
Erick Arostegui Cunza <scorpius86@hotmail.com>
2020-10-28 19:08:38 -0500

    Session 02

---
+++ 01 - Net Core Fundamentals/AspNet.Fundamentals/AspNetFundamentals.MVC.Console/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetFundamentals.MVC.Console.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
