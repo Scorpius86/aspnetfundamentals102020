5a9bd067eb2aed7f61b4ca63c17639cf56164b70
Erick Arostegui Cunza <scorpius86@hotmail.com>
2020-11-04 21:34:01 -0500

    session 04

---
+++ 04 - Net Core Fundamentals/NetCore.Fundamentals/NetCore.Fundamentals.Client.Web/Migrations/20201105022854_RemoveCustomField.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NetCore.Fundamentals.Client.Web.Migrations
+{
+    public partial class RemoveCustomField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomField",
+                table: "Usuario");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CustomField",
+                table: "Usuario",
+                type: "nchar(10)",
+                fixedLength: true,
+                maxLength: 10,
+                nullable: true);
+        }
+    }
+}
---
+++ 04 - Net Core Fundamentals/NetCore.Fundamentals/NetCore.Fundamentals.Client.Web/Models/PostViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.Fundamentals.Client.Web.Models
+{
+    public class PostViewModel
+    {
+        public int PostId { get; set; }
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+        public int UsuarioIdPropietario { get; set; }
+        public int UsuarioIdCreacion { get; set; }
+        [Display(Name = "Fecha Creación")]
+        public DateTime FechaCreacion { get; set; }
+        public int UsuarioIdActualizacion { get; set; }
+        [Display(Name = "Fecha Actualización")]
+        public DateTime FechaActualizacion { get; set; }
+
+        [Display(Name = "Usuario Actualizacón")]
+        public virtual UsuarioViewModel UsuarioActualizacion { get; set; }
+        [Display(Name = "Usuario Creación")]
+        public virtual UsuarioViewModel UsuarioCreacion { get; set; }
+        [Display(Name = "Propietario")]
+        public virtual UsuarioViewModel UsuarioPropietario { get; set; }        
+    }
+}
