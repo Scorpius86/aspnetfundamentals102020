bd9f80416506fb0ed83da5d4128dad84990025ec
Erick Arostegui Cunza <scorpius86@hotmail.com>
2020-11-06 19:06:07 -0500

    Session 05

---
+++ 05 - Net Core Fundamentals/01/NetCore.Fundametals.Security/NetCore.Fundametals.Security.Data/Repositories/IConferenceRepository.cs
@@ -0,0 +1,12 @@
+﻿using NetCore.Fundamentals.Security.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace NetCore.Fundamentals.Security.Data.Repositories
+{
+    public interface IConferenceRepository
+    {
+        Task<int> Add(ConferenceDto conferenceDto);
+        Task<List<ConferenceDto>> GetAll();
+        Task<ConferenceDto> GetById(int conferenceId);
+    }
+}
---
+++ 05 - Net Core Fundamentals/01/NetCore.Fundametals.Security/NetCore.Fundametals.Security.Dtos/AttendeeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCore.Fundamentals.Security.Dtos
+{
+    public class AttendeeDto
+    {
+        public int AttendeeId { get; set; }
+        public int ConferenceId { get; set; }
+        public string Name { get; set; }
+    }
+}
