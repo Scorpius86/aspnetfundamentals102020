06d4c8476f95feeafa433fd1551dd1cf3fec1058
Erick Arostegui Cunza <scorpius86@gmail.com>
2020-11-18 19:11:31 -0500

    Session 07

---
+++ 05 - Net Core Fundamentals/03/NetCore.Fundamentals.Security/NetCore.Fundametals.Security.Client.Web/Areas/Identity/EmailSender.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCore.Fundametals.Security.Client.Web.Areas.Identity
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly string _apiKey;
+        private readonly string _fromName;
+        private readonly string _fromEmail;
+        public EmailSender(IConfiguration configuration)
+        {
+            _apiKey = configuration["SendGrid:ApiKey"];
+            _fromName = configuration["SendGrid:FromName"];
+            _fromEmail = configuration["SendGrid:FromEmail"];
+        }
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            //var client = new SendGridClient(_apiKey);
+            //var msg = new SendGridMessage()
+            //{
+            //    From = new EmailAddress(_fromEmail, _fromName),
+            //    Subject = subject,
+            //    PlainTextContent = message,
+            //    HtmlContent = message
+            //};
+
+
+            
+            //msg.AddTo(new EmailAddress(email));
+            ////Disable click tracking
+            //msg.SetClickTracking(false, false);
+
+            //var response = await client.SendEmailAsync(msg);
+
+
+            var apiKey = _apiKey;
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(_fromEmail, _fromName);            
+            var to = new EmailAddress(email);
+            var plainTextContent = message;
+            var htmlContent = message;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+
+        }
+    }
+}
---
+++ 05 - Net Core Fundamentals/04/NetCore.Fundamentals.OIC/NetCore.Fundamentals.OIC.IdentityProvider/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore.Fundamentals.OIC.IdentityProvider.Areas.Identity.Data;
+using NetCore.Fundamentals.OIC.IdentityProvider.Data;
+
+[assembly: HostingStartup(typeof(NetCore.Fundamentals.OIC.IdentityProvider.Areas.Identity.IdentityHostingStartup))]
+namespace NetCore.Fundamentals.OIC.IdentityProvider.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityProviderContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityProviderContextConnection")));
+
+                services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+                    options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<IdentityProviderContext>()
+                    .AddDefaultUI()
+                    .AddDefaultTokenProviders();
+
+                services.AddScoped<IUserClaimsPrincipalFactory<ApplicationUser>,
+                    ApplicationUserClaimsPrincipalFactory>();
+                services.AddTransient<IEmailSender, EmailSender>();
+
+                services.AddAuthentication()
+                    .AddGoogle(opt =>
+                    {
+                        opt.ClientId = context.Configuration["Google:ClientId"];
+                        opt.ClientSecret = context.Configuration["Google:ClientSecret"];
+                    });
+            });
+        }
+    }
+}
